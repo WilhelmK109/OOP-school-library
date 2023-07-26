fd7c7fd599c2f640271d7c034a0213ddf46ddef1
WilhelmK109 <kamulungaw@gmail.com>
2023-07-26 22:09:42 +0200

    Create a class Book

---
+++ book.rb
@@ -0,0 +1,16 @@
+require_relative 'rental'
+
+class Book
+  attr_accessor :title, :author, :rentals
+
+  def initialize(title, author)
+    @title = title
+    @author = author
+    @rentals = []
+  end
+
+  def add_rental(person, date)
+    rental = Rental.new(person, date, self)
+    @rentals << rental
+  end
+end
