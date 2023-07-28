f1cb9863a937977b77610bd7e9308728af0a2556
WilhelmK109 <kamulungaw@gmail.com>
2023-07-28 18:23:58 +0200

    Create main.rb to define the entry point

--- main.rb
+++ main.rb
@@ -1,12 +1,8 @@
-require_relative 'person'
-require_relative 'capitalize_decorator'
-require_relative 'trimmer_decorator'
+require_relative 'app'
 
-person = Person.new(22, name: 'maximilianus')
-puts person.correct_name
+def entry_point
+  app = App.new
+  app.start
+end
 
-capitalized_person = CapitalizeDecorator.new(person)
-puts capitalized_person.correct_name
-
-capitalized_trimmed_person = TrimmerDecorator.new(capitalized_person)
-puts capitalized_trimmed_person.correct_name
+entry_point if $PROGRAM_NAME == __FILE__
--- teacher.rb
+++ teacher.rb
@@ -4,7 +4,7 @@ class Teacher < Person
   attr_accessor :specialization
 
   def initialize(specialization, age, parent_permission: true, name: 'Unknown')
-    super(age, parent_permission, name)
+    super(age, parent_permission: parent_permission, name: name)
     @specialization = specialization
   end
 
